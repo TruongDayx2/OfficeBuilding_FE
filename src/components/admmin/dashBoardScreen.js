4d4eca29707c6c2d5ba949384527f6e815755927
Nolan <92731646+Blood-NT@users.noreply.github.com>
2023-10-20 21:21:55 +0700

    update
    user

---
+++ src/components/admmin/dashBoardScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './admin.css';
+function DashboardScreen() {
+    return (
+        <div>
+            <h1>Dashboard Screen</h1>
+            <p>This is the dashboard screen.</p>
+        </div>
+    );
+}
+
+export default DashboardScreen;
---
+++ src/components/admmin/userScreen.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import './admin.css';
+
+function UsersScreen() {
+    const [showPopup, setShowPopup] = useState(false);
+    const handleAdduser = () => {
+        console.log("Thêm user");
+        console.log(showPopup);
+        setShowPopup(showPopup => !showPopup);
+    }
+    const closePopup = () => {
+        setShowPopup(false);
+    }
+    const handleEdituser = (id) => {
+        alert("Sửa user" + users.find(user => user.id === id).name);
+    }
+    const handleDeleteuser = (id) => {
+        alert("Xóa user" + users.find(user => user.id === id).name);
+    }
+    const addUser = () => {
+        alert("Thêm user");
+    }
+
+    const users = [
+        {
+            id: 1,
+            name: "Nguyễn Văn A",
+            email: "nguyenvana@gmail.com",
+            role: "admin"
+        },
+        {
+            id: 2,
+            name: "Nguyễn Văn B",
+            email: "nguyenvanb@gmail.com",
+            role: "user"
+        }
+    ]
+
+
+
+    return (
+        <div>
+
+            <div>
+                <div style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}>
+                    <h1> Quản lý user</h1>
+                    <button className="btn btn-primary" onClick={handleAdduser}>Thêm user</button>
+                </div>
+
+                <br></br>
+                <div>
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>STT</th>
+                                <th>Tên</th>
+                                <th>Email</th>
+                                <th>Quyền</th>
+                                <th>Thao tác</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.role}</td>
+                                        <td>
+                                            <button className="btn btn-primary" onClick={() => handleEdituser(user.id)}>Sửa</button>
+                                            <button className="btn btn-danger" onClick={() => handleDeleteuser(user.id)}>Xóa</button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+
+
+            </div>
+
+            {showPopup && (
+                <div className="popup_dashboard" >
+                    <div className="popup-content" style={{ display: "inline-list-item" }}>
+                        <h2>Thêm user</h2>
+                        <div>
+
+                            <input type="text" placeholder="Tên user" />
+                        </div>
+                        <div>
+
+                            <input type="text" placeholder="userName" />
+                        </div>
+                        <div>
+
+                            <input type="text" placeholder="Email" />
+                        </div>
+                        <div>
+
+                            <input type="text" placeholder="Password" />
+                        </div>
+                        <div>
+
+                            <input type="text" placeholder="nhập lại Password" />
+                        </div>
+                        <select>
+                            <option value="admin">Admin</option>
+                            <option value="user">User</option>
+                        </select>
+                        <div className="btn-popup" style={{ justifyContent: "space-evenly", display: "flex" }}>
+                            <button className="btn btn-primary" onClick={() => addUser()}>Thêm</button>
+                            <button className="btn btn-danger" onClick={closePopup}>Đóng</button>
+                        </div>
+
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+
+
+export default UsersScreen;
